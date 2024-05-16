d325b211ed9faaaa3afce8c68226297a0894c5d0
mpsuryamp <appusuryamp8281@gmail.com>
2024-05-16 15:04:34 +0530

    Add files via upload

---
+++ largest number.py
@@ -0,0 +1,16 @@
+# a=[15,73,35,46,100]
+# b=sorted(a)
+# print(b)
+# sl=46
+# larg=73
+# for i in a:
+#     if i>larg:
+#         sl=larg
+#         larg=i
+# print(sl)        
+
+rows=6
+for i in range (rows):
+    for j in range(i):
+        print(i,end=' ')
+    print(" ")    
